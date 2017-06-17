63a91875506d377d8ecc3d23ac62b05d8872fe9c
Moritz Göckel <moritz.goeckel@gmail.com>
2017-06-18 00:39:39 +0200

    Experimenting with restserver

--- Start/StartExportServer.js
+++ Start/StartExportServer.js
@@ -12,15 +12,21 @@ let exporter = new Exporter(es);
 let exp = Express();
 let rest = ExpressRest(exp);
 
+let time = new TimeHelper();
+
 //exp.use("/", Express.static(__dirname + '/frontend'));
 
 rest.get('/export', function(req, rest) {
-    console.log(req.params);
-    console.log(req.query);
-    
-    exporter.getDownsampledArray(0, 150, "Inst", 10, function(res){
-        return rest.ok(res);
+    if(req.query.from == undefined || req.query.to == undefined || req.query.instrument == undefined || req.query.interval == undefined)
+        return rest.ok({query:req.query, result:"missing parameters"});
+
+    exporter.getDownsampledArray(req.query.from, req.query.to, req.query.instrument, req.query.interval, function(res){
+        return rest.ok({query:req.query, result:"ok", data:res});
     });
+});
+
+rest.get('/time', function(req, rest) {
+    return rest.ok({now:time.getUTCDate(), ago48h:time.getUTCDate() - (48 * 60 * 60 * 1000), ago24h:time.getUTCDate() - (24 * 60 * 60 * 1000), ago96h:time.getUTCDate() - (96 * 60 * 60 * 1000)});
 });
 
 let listener = exp.listen(this.port, function(){
